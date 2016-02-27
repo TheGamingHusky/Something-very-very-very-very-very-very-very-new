4bbceed435120d2e6d6aba0587d0186ac43c33d1
TheGamingHusky <andrei.proctor@gmail.com>
2016-02-26 17:52:20 -0800

    Create List.py

---
+++ List.py
@@ -0,0 +1,8 @@
+# import the magic random picker
+
+# list of things to pick from
+typing_options = ['Hi','bye']
+# find a way to pick something
+typing_choice = typing_options [1]
+# a way to tell the user what to do
+print typing_choice
